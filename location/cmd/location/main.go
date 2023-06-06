b1a38a1d72b77641de31a1ece3108a5d4e2f91ca
Folevo <folevo1985@gmail.com>
2023-06-06 17:56:19 +0300

    Fire Kafka event with latest courier position

--- location/cmd/location/main.go
+++ location/cmd/location/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/Sharykhin/go-delivery-dymas/location/domain"
+	"github.com/Sharykhin/go-delivery-dymas/location/env"
 	"github.com/Sharykhin/go-delivery-dymas/location/http"
 	"github.com/Sharykhin/go-delivery-dymas/location/http/handler"
 	"github.com/Sharykhin/go-delivery-dymas/location/kafka"
@@ -9,38 +10,13 @@ import (
 	"github.com/Shopify/sarama"
 	"github.com/gorilla/mux"
 	"log"
-	"os"
-	"strconv"
 )
 
 func main() {
-	address := "localhost:9092"
-	if os.Getenv("KAFKA_BROKERS") != "" {
-		address = os.Getenv("KAFKA_BROKERS")
-	}
-	host := "localhost"
-	port := "6379"
-	db := 0
+	address := env.GetKafkaConfig()
 	var addr string
-	addr = host + ":" + port
-	if os.Getenv("REDIS_HOST") != "" && os.Getenv("REDIS_PORT") != "" {
-		host = os.Getenv("REDIS_HOST")
-		port = os.Getenv("REDIS_PORT")
-		addr = host + ":" + port
-	}
-
-	if os.Getenv("DB_REDIS") != "" {
-		dbConvert, error := strconv.Atoi(os.Getenv("DB_REDIS"))
-		db = dbConvert
-		if error != nil {
-			panic(error)
-		}
-	}
-	portServer := ":8081"
-
-	if os.Getenv("HTTP_PORT") != "" {
-		portServer = os.Getenv("HTTP_PORT")
-	}
+	addr, db := env.GetRedisConfig()
+	portServer := env.GetServerEnv()
 	publisher := kafka.NewPublisher(sarama.NewConfig(), address)
 	client := redis.CreateConnect(addr, db)
 	repo := redis.CreateCouriersRepository(client)
