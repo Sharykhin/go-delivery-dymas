17b3319504355cf2dfb53478fc8b5e561ec1c48a
Folevo <folevo1985@gmail.com>
2023-07-27 16:50:38 +0300

    Create consumer to store all the courier locations

--- location/env/config.go
+++ location/env/config.go
@@ -8,9 +8,9 @@ type Config struct {
 	Address    string `env:"KAFKA_BROKERS" envDefault:"localhost:9092"`
 	Addr       string `env:"REDIS_ADDRESS" envDefault:"localhost:6379"`
 	PortServer string `env:"PORT_SERVER" envDefault:"8081"`
-	Assignor   string `env:"ASSIGNOR" envDefault:"range"`
-	Oldest     bool   `env:"OLDEST" envDefault:"true"`
-	Verbose    bool   `env:"VERBOSE" envDefault:"false"`
+	Assignor   string `env:"KAFKA_CONSUMER_ASSIGNOR" envDefault:"range"`
+	Oldest     bool   `env:"KAFKA_CONSUMER_OLDEST" envDefault:"true"`
+	Verbose    bool   `env:"KAFKA_CONSUMER_VERBOSE" envDefault:"false"`
 	DbName     string `env:"POSTGRES_DB" envDefault:"courier_location"`
 	PasswordDb string `env:"POSTGRES_PASSWORD" envDefault:"S3cret"`
 	DbUser     string `env:"POSTGRES_USER" envDefault:"citizix_user"`
