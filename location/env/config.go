6ba561cc17dec20d0f455a13fcadd4e588a0b811
Folevo <folevo1985@gmail.com>
2023-07-27 18:14:38 +0300

    Create consumer to store all the courier locations

--- location/env/config.go
+++ location/env/config.go
@@ -8,6 +8,7 @@ type Config struct {
 	Address    string `env:"KAFKA_BROKERS" envDefault:"localhost:9092"`
 	Addr       string `env:"REDIS_ADDRESS" envDefault:"localhost:6379"`
 	PortServer string `env:"PORT_SERVER" envDefault:"8081"`
+	Db         int    `env:"DB_REDIS" envDefault:"0"`
 	Assignor   string `env:"KAFKA_CONSUMER_ASSIGNOR" envDefault:"range"`
 	Oldest     bool   `env:"KAFKA_CONSUMER_OLDEST" envDefault:"true"`
 	Verbose    bool   `env:"KAFKA_CONSUMER_VERBOSE" envDefault:"false"`
