a29369aff75dd8c8405d3d2b3a7620ab9bb7ff68
Folevo <folevo1985@gmail.com>
2023-07-27 16:45:37 +0300

    Create consumer to store all the courier locations

--- location/cmd/track/main.go
+++ location/cmd/track/main.go
@@ -13,19 +13,16 @@ func main() {
 	config, err := env.GetConfig()
 	if err != nil {
 		log.Printf("failed to parse variable env: %v\n", err)
-		return
 	}
 	ctx := context.Background()
 	repo, err := postgres.NewCourierLocationRepository(config.DbName, config.DbUser, config.PasswordDb)
 	defer repo.Client.Close()
 	if err != nil {
 		log.Printf("failed to parse variable env: %v\n", err)
-		return
 	}
 	consumerGroup, err := kafka.NewCourierLocationConsumer(repo, config.Address, config.Verbose, config.Oldest, config.Assignor)
 	if err != nil {
 		log.Println(err)
-		return
 	}
 	err = consumerGroup.ConsumeCourierLatestCourierGeoPositionMessage(ctx)
 	if err != nil {
