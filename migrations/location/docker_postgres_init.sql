02c08e17a068b022ba488a34badd4ec173460e22
Folevo <folevo1985@gmail.com>
2023-07-27 18:08:01 +0300

    Store_courier_geo_position_in_Redis: Add error handle

--- migrations/location/docker_postgres_init.sql
+++ migrations/location/docker_postgres_init.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS courier_location;
 GRANT ALL PRIVILEGES ON DATABASE courier_location TO citizix_user;
-
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS courier_latest_cord (
-                                          courier_id VARCHAR(36) PRIMARY KEY,
+                                          courier_id UUID,
                                           latitude double precision,
                                           longitude double precision,
                                           created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
