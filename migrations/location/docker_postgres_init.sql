828a75ab508820a74f62d4b5f61cb3621f622da7
Folevo <folevo1985@gmail.com>
2023-08-16 18:47:58 +0300

    Create consumer to store all the courier locations

--- migrations/location/docker_postgres_init.sql
+++ migrations/location/docker_postgres_init.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS courier_latest_cord (
                                           courier_id UUID NOT NULL,
                                           latitude double precision NOT NULL,
                                           longitude double precision NOT NULL ,
-                                          created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                                          created_at TIMESTAMPTZ NOT NULL,
                                           PRIMARY KEY (courier_id, created_at)
 );
