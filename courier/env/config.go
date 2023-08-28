62577146444d6912916c6edc469867a22bce0dbf
Folevo <folevo1985@gmail.com>
2023-08-28 22:37:48 +0300

    add db connection

--- courier/env/config.go
+++ courier/env/config.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Config struct {
-	DbName       string `env:"POSTGRES_DB" envDefault:"courier_location"`
-	DbPassword   string `env:"POSTGRES_PASSWORD" envDefault:"S3cret"`
-	DbUser       string `env:"POSTGRES_USER" envDefault:"citizix_user"`
-	PortServerCourier   string `env:"PORT_SERVER_COURIER" envDefault:"8881"`
+	DbName            string `env:"POSTGRES_DB" envDefault:"couriers"`
+	DbPassword        string `env:"POSTGRES_PASSWORD" envDefault:"S3cret"`
+	DbUser            string `env:"POSTGRES_USER" envDefault:"citizix_user"`
+	PortServerCourier string `env:"PORT_SERVER_COURIER" envDefault:"8881"`
 }
 
 func GetConfig() (config Config, err error) {
--- migrations/courier/courier_create_table.sql
+++ migrations/courier/courier_create_table.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE IF NOT EXISTS courier_location;
+CREATE DATABASE IF NOT EXISTS couriers;
 GRANT ALL PRIVILEGES ON DATABASE courier_location TO citizix_user;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS couriers (
-                                          ID UUID NOT NULL,
-                                          FirstName char(30) NOT NULL,
-                                          IsAvailable BOOLEAN NOT NULL,
-                                          PRIMARY KEY (ID)
+                                          id UUID NOT NULL,
+                                          first_name char(30) NOT NULL,
+                                          is_available BOOLEAN NOT NULL,
+                                          PRIMARY KEY (id)
 );
