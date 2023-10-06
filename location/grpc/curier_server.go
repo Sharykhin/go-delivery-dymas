b1212f0c8872a78bf2f65dfd683285d44d96a4c7
Folevo <folevo1985@gmail.com>
2023-10-06 23:48:32 +0300

    fix code review

--- location/grpc/curier_server.go
+++ location/grpc/curier_server.go
@@ -16,10 +16,11 @@ type CourierServer struct {
 
 func (courierServer CourierServer) GetCourierLatestPosition(ctx context.Context, req *pb.GetCourierLatestPositionRequest) (*pb.GetCourierLatestPositionResponse, error) {
 	courierLatestPosition, err := courierServer.CourierLocationRepository.GetLatestPositionCourierById(ctx, req.CourierId)
+
 	if err != nil {
 		return nil, status.Errorf(
-			codes.Internal,
-			fmt.Sprintf("Internal error: %v", err),
+			codes.NotFound,
+			fmt.Sprintf("Position Not found: %v", err),
 		)
 	}
 
