dd91350ad6030612a2edc393d1db81431ba51d6d
Folevo <folevo1985@gmail.com>
2024-02-14 13:32:57 +0300

    change grpc on kafka

---
+++ courier/kafka/courier_consumer.go
@@ -0,0 +1,80 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/IBM/sarama"
+	"github.com/Sharykhin/go-delivery-dymas/courier/domain"
+)
+
+// OrderTopic where we have message with different event for order
+const OrderTopic = "orders"
+
+// OrderConsumer gets order from kafka and apply order to courier and send order message validations
+type OrderConsumer struct {
+	courierRepository        domain.CourierRepositoryInterface
+	orderValidationPublisher domain.OrderValidationPublisher
+}
+
+// Customer needs for order message
+type Customer struct {
+	PhoneNumber string `json:"phone_number"`
+}
+
+// OrderPayload  needs for order message
+type OrderPayload struct {
+	OrderID  string   `json:"id"`
+	Customer Customer `json:"customer"`
+}
+
+// OrderMessage will consume, when order create and publish in queue.
+type OrderMessage struct {
+	OrderPayload OrderPayload `json:"payload"`
+	Event        string       `json:"event"`
+}
+
+// NewOrderConsumer creates and init order consumer this consumer consume message from kafka
+func NewOrderConsumer(
+	courierRepository domain.CourierRepositoryInterface,
+	orderValidationPublisher domain.OrderValidationPublisher,
+) *OrderConsumer {
+	courierConsumer := &OrderConsumer{
+		courierRepository:        courierRepository,
+		orderValidationPublisher: orderValidationPublisher,
+	}
+
+	return courierConsumer
+}
+
+// HandleJSONMessage Handle kafka message in json format
+func (orderConsumer *OrderConsumer) HandleJSONMessage(ctx context.Context, message *sarama.ConsumerMessage) error {
+	var orderMessage OrderMessage
+	if err := json.Unmarshal(message.Value, &orderMessage); err != nil {
+		log.Printf("failed to unmarshal Kafka message into courier location struct: %v\n", err)
+
+		return nil
+	}
+
+	if orderMessage.Event == "updated" {
+		log.Printf("Order assign success")
+
+		return nil
+	}
+
+	courierAssigment, err := orderConsumer.courierRepository.AssignOrderToCourier(ctx, orderMessage.OrderPayload.OrderID)
+	fmt.Println(err)
+	if err != nil {
+		return fmt.Errorf("failed to save a courier assigments in the repository: %w", err)
+	}
+
+	err = orderConsumer.orderValidationPublisher.PublishOrderValidation(ctx, &courierAssigment)
+
+	if err != nil {
+		return fmt.Errorf("failed to publish a order message in kafka: %w", err)
+	}
+
+	return nil
+}
---
+++ courier/kafka/order_publisher.go
@@ -0,0 +1,52 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/Sharykhin/go-delivery-dymas/courier/domain"
+	pkgkafka "github.com/Sharykhin/go-delivery-dymas/pkg/kafka"
+)
+
+const OrderTopicValidation = "order_validations"
+
+// OrderValidationPublisher publisher for kafka
+type OrderValidationPublisher struct {
+	publisher *pkgkafka.Publisher
+}
+
+// NewOrderValidationPublisher creates new publisher and init
+func NewOrderValidationPublisher(publisher *pkgkafka.Publisher) *OrderValidationPublisher {
+	orderValidationPublisher := OrderValidationPublisher{
+		publisher: publisher,
+	}
+
+	return &orderValidationPublisher
+}
+
+// PublishOrderValidation sends order message in json format in Kafka.
+func (orderPublisher *OrderValidationPublisher) PublishOrderValidation(ctx context.Context, courierAssigment *domain.CourierAssignments) error {
+	messageOrder := domain.OrderMessageValidation{
+		IsSuccessful: true,
+		ServiceName:  "courier",
+		Payload: domain.CourierAssignments{
+			OrderID:   courierAssigment.OrderID,
+			CourierID: courierAssigment.CourierID,
+		},
+	}
+
+	message, err := json.Marshal(messageOrder)
+
+	if err != nil {
+		return fmt.Errorf("failed to marshal order before sending Kafka event: %w", err)
+	}
+
+	err = orderPublisher.publisher.PublishMessage(ctx, message, []byte(courierAssigment.OrderID))
+
+	if err != nil {
+		return fmt.Errorf("failed to publish order event: %w", err)
+	}
+
+	return nil
+}
