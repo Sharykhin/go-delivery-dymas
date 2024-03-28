ba46244a6f84cd7867dc67529c3e9c7efd7c00d3
Folevo <folevo1985@gmail.com>
2024-03-28 18:49:52 +0300

    add make command

---
+++ location/db/migrations/20240328074432_location_create_table.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS courier_latest_cord (
+               courier_id UUID NOT NULL,
+               latitude double precision NOT NULL,
+               longitude double precision NOT NULL ,
+               created_at TIMESTAMPTZ NOT NULL,
+               PRIMARY KEY (courier_id, created_at)
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP DATABASE courier_latest_cord;
+-- +goose StatementEnd
