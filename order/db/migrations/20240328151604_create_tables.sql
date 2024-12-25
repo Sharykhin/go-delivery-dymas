9f92c63445f011d174a329e7cdcd6b6b1574df1a
folevo <folevo1985@gmail.com>
2024-12-25 13:41:20 +0300

    Add cancel order

--- order/db/migrations/20240328151604_create_tables.sql
+++ order/db/migrations/20240328151604_create_tables.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE orders (
+CREATE TABLE IF NOT EXISTS orders (
     id UUID DEFAULT gen_random_uuid(),
     courier_id UUID NULL,
     customer_phone_number char(15) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE orders (
     PRIMARY KEY (id)
     );
 
-CREATE TABLE order_validations (
+CREATE TABLE IF NOT EXISTS order_validations (
     order_id UUID NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
     courier_validated_at TIMESTAMPTZ,
     courier_error VARCHAR(256),
